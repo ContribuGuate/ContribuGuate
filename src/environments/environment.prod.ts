5a4904cfdcfde10210bc58c2b7b1508ed8764a9a
Emilio Hernandez <emiliohernandezg7@icloud.com>
2024-09-14 09:54:35 -0600

    firebase deploy ready

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:3000/api/contribuguate/v1/'
+  baseUrl: 'https://contribuguatebe.onrender.com/api/contribuguate/v1/'
 };
