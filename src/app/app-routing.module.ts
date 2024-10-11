c7762e94f06292d05ac35216029f3ff6edc998d9
DylanBatzin <dbatzina@miumg.edu.gt>
2024-10-11 16:04:33 -0600

    Vista Social

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { NotfoundComponent } from './demo/components/notfound/notfound.component';
 import { AppLayoutComponent } from "./layout/app.layout.component";
 import { AuthGuard } from './demo/guards/auth.guard';
+import { SocialComponent } from './demo/components/social/social.component';  // Asegúrate de que esta ruta sea correcta
 
 @NgModule({
     imports: [
@@ -22,8 +23,11 @@ import { AuthGuard } from './demo/guards/auth.guard';
                 canActivate: [AuthGuard]
             },
             { path: 'auth', loadChildren: () => import('./demo/components/auth/auth.module').then(m => m.AuthModule) },
-            { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
             { path: 'notfound', component: NotfoundComponent },
+            {
+                path: 'social',  
+                component: SocialComponent  
+            },
             { path: '**', redirectTo: '/notfound' },
         ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' })
     ],
