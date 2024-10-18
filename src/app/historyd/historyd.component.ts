b9898a93090edf4b987099fec485d50a38ee8c34
DylanBatzin <dbatzina@miumg.edu.gt>
2024-10-18 17:05:36 -0600

    Historial

---
+++ src/app/historyd/historyd.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historyd',
+  standalone: true,
+  imports: [],
+  templateUrl: './historyd.component.html',
+  styleUrl: './historyd.component.scss'
+})
+export class HistorydComponent {
+
+}
